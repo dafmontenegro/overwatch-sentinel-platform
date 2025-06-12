7dafc67882eb8eef9c2e931d1cd1b2d705e071e3
breynerciro <bciro@unal.edu.co>
2025-06-12 16:11:24 -0500

    feat: Añadir configuración del servidor y archivos de entrada para la aplicación React con SSR

--- osp-frontend-web/src/App.tsx
+++ osp-frontend-web/src/App.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import AppRoutes from './routes/AppRoutes';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <AuthProvider>
-        <div className="min-h-screen flex flex-col">
-          <AppRoutes />
-        </div>
-      </AuthProvider>
-    </Router>
+    <AuthProvider>
+      <div className="min-h-screen flex flex-col">
+        <AppRoutes />
+      </div>
+    </AuthProvider>
   );
 };
 
---
+++ osp-frontend-web/src/entry-client.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { hydrateRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css'
+import App from './App';
+
+hydrateRoot(document.getElementById('root')!,
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
